3c9202de5bd50928dde08c6ff1045ef09d486aec
SakiiR SakiiR <sakiirlessons@gmail.com>
2021-01-06 10:48:38 +0100

    Encoding the slug input to hex if the slugify function returns and empty string

--- api/src/utils/slugify.ts
+++ api/src/utils/slugify.ts
@@ -1,5 +1,19 @@
 import slugify from "slugify";
 
+function utf8ToHex(str) {
+  return Array.from(str)
+    .map((c: string) =>
+      c.charCodeAt(0) < 128 ? c.charCodeAt(0).toString(16) : encodeURIComponent(c).replace(/\%/g, "").toLowerCase()
+    )
+    .join("");
+}
+
 export default function makeSlug(text: string): string {
-  return slugify(text, { strict: true, lower: true });
+  const slug = slugify(text, { strict: true, lower: true });
+
+  if (!slug) {
+    return utf8ToHex(text);
+  }
+
+  return slug;
 }
