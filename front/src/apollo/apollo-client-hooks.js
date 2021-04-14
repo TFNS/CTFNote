75b3cdc2fa85befc7b6bf7a8f98a463302ad1998
BitK <bitk@yeswehack.com>
2021-04-14 22:12:16 +0200

    Only use jwt when available, this will avoid unessary error

--- front/src/apollo/apollo-client-hooks.js
+++ front/src/apollo/apollo-client-hooks.js
@@ -7,59 +7,71 @@ import { getMainDefinition } from "apollo-utilities";
 import { onError } from "apollo-link-error";
 import { Notify } from "quasar";
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message }) => {
-      Notify.create({ message, type: "negative" });
-    });
-  }
-  if (networkError) {
-    Notify.create({ message: `[Network error]: ${networkError}`, type: "negative" });
-  }
-});
-
-const httpLinkConfig = {
-  uri: "/graphql"
-};
-
-const wsLinkConfig = {
-  uri: `${location.protocol == "https:" ? "wss" : "ws"}://${location.host}/graphql`,
-  options: {
-    reconnect: true,
-    connectionParams: {
-      authorization: `Bearer ${localStorage.getItem("JWT")}`
+function createErrorLink() {
+  return onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message }) => {
+        Notify.create({ message, type: "negative" });
+      });
     }
-  }
-};
-
-const httpLink = new HttpLink(httpLinkConfig);
-const wsLink = new WebSocketLink(wsLinkConfig);
-
-const terminatingLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return definition.kind === "OperationDefinition" && definition.operation === "subscription";
-  },
-  wsLink,
-  httpLink
-);
-
-const authLink = new ApolloLink((operation, forward) => {
-  operation.setContext(({ headers = {} }) => {
-    const token = localStorage.getItem("JWT");
-
-    if (token) {
-      headers = { ...headers, authorization: `Bearer ${token}` };
+    if (networkError) {
+      Notify.create({ message: `[Network error]: ${networkError.message}`, type: "negative" });
     }
-
-    return { headers };
   });
+}
 
-  return forward(operation);
-});
+function createWSLink() {
+  const connectionParams = {};
+  const jwt = localStorage.getItem("JWT");
+  if (jwt) {
+    connectionParams.authorization = `Bearer ${jwt}`;
+  }
+  const wsLinkConfig = {
+    uri: `${location.protocol == "https:" ? "wss" : "ws"}://${location.host}/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams
+    }
+  };
+
+  return new WebSocketLink(wsLinkConfig);
+}
+
+function createHTTPLink() {
+  return new HttpLink({
+    uri: "/graphql"
+  });
+}
+
+function createTerminatingLink() {
+  return split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+    },
+    createWSLink(),
+    createHTTPLink()
+  );
+}
+
+function createAuthLink() {
+  return new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers = {} }) => {
+      const token = localStorage.getItem("JWT");
+
+      if (token) {
+        headers = { ...headers, authorization: `Bearer ${token}` };
+      }
+
+      return { headers };
+    });
+
+    return forward(operation);
+  });
+}
 
 export async function apolloClientBeforeCreate({ apolloClientConfigObj }) {
-  apolloClientConfigObj.link = ApolloLink.from([errorLink, authLink, terminatingLink]);
+  apolloClientConfigObj.link = ApolloLink.from([createErrorLink(), createAuthLink(), createTerminatingLink()]);
 }
 
 let apollo = {};
