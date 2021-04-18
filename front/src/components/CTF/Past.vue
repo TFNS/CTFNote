42e9e157ef5984651bd02b5adf8fd34a5797e7e5
SakiiR <sakiirlessons@gmail.com>
2021-04-18 23:10:34 +0200

    Added basic pagination on the past ctf view

--- front/src/components/CTF/Past.vue
+++ front/src/components/CTF/Past.vue
@@ -4,10 +4,11 @@
       <q-table
         no-data-label="No CTF available."
         flat
-        :pagination="pagination"
+        :pagination.sync="pagination"
         :data="ctfs"
-        :loading="$apollo.queries.ctfs.loading"
+        :loading="loading"
         :columns="columns"
+        @request="onRequest"
       >
         <template #body-cell-title="props">
           <q-td :props="props">
@@ -28,27 +29,64 @@
 import db from "src/gql";
 import { getDateTime } from "src/utils";
 
+const MAX_PER_PAGE = 100;
+
 export default {
-  data() {
-    const pagination = { rowsPerPage: 20 };
-    const columns = [
-      { name: "title", classes: "text-center", align: "center", sortable: true, label: "Title", field: "title" },
-      {
-        name: "data",
-        align: "right",
-        sortable: true,
-        label: "Date",
-        field: task => task.startTime,
-        format: t => getDateTime(t)
-      }
-    ];
-    return { columns, pagination };
+  mounted() {
+    this.onRequest({ pagination: this.pagination });
   },
-  apollo: {
-    ctfs: {
-      query: db.ctf.PAST,
-      update: data => data.pastCtf.nodes
-    }
-  }
+  data() {
+    return {
+      columns: [
+        { name: "title", classes: "text-center", align: "center", sortable: true, label: "Title", field: "title" },
+        {
+          name: "data",
+          align: "right",
+          sortable: true,
+          label: "Date",
+          field: (task) => task.startTime,
+          format: (t) => getDateTime(t),
+        },
+      ],
+      pagination: { rowsNumber: 500, rowsPerPage: 15 },
+      ctfs: [],
+      loading: false,
+    };
+  },
+  methods: {
+    async onRequest(props) {
+      const { rowsPerPage } = props.pagination;
+
+      let page = props.pagination.page;
+
+      if (page === void 0) {
+        page = 1;
+      }
+      page--;
+
+      let first = rowsPerPage > MAX_PER_PAGE ? MAX_PER_PAGE : rowsPerPage;
+
+      if (first == 0) first = MAX_PER_PAGE;
+
+      const offset = page * first;
+
+      this.loading = true;
+      const {
+        data: {
+          pastCtf: { totalCount, nodes: ctfs },
+        },
+      } = await this.$apollo.mutate({
+        mutation: db.ctf.PAST,
+        variables: { first, offset },
+      });
+      this.loading = false;
+
+      this.ctfs = ctfs;
+
+      this.pagination.rowsNumber = totalCount;
+      this.pagination.rowsPerPage = first;
+      this.pagination.page = page + 1;
+    },
+  },
 };
 </script>
