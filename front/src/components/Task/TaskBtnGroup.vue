fde3fc37483665bef156c0317947cf203abe5fc7
BitK <bitk@yeswehack.com>
2021-11-28 18:03:25 +0100

    Added Table display
    Added a new display mode: table
    The components now use injected function instead of event.
    This make the code much cleaner by avoiding a lot of event reemission
    Tasks buttons, category chip and player list
    are moved to their own component to avoid repetition

---
+++ front/src/components/Task/TaskBtnGroup.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="q-gutter-sm">
+    <q-btn
+      round
+      size="sm"
+      :title="onItTitle"
+      :icon="onItIcon"
+      :color="onItColor"
+      @click="updateOnIt(!onIt)"
+    />
+    <q-btn
+      round
+      size="sm"
+      title="Enter flag"
+      icon="flag"
+      color="positive"
+      @click="solveTask(task)"
+    />
+    <q-btn
+      round
+      size="sm"
+      :title="`Edit ${task.title}`"
+      icon="edit"
+      color="warning"
+      @click="editTask(task)"
+    />
+    <q-btn
+      round
+      size="sm"
+      :title="`Delete ${task.title}`"
+      icon="delete"
+      color="negative"
+      @click="deleteTask(task)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import ctfnote from 'src/ctfnote';
+import { Task } from 'src/ctfnote/models';
+import { injectStrict } from 'src/ctfnote/utils';
+import { defineComponent } from 'vue';
+import keys from './injectionKeys';
+
+export default defineComponent({
+  props: {
+    task: { type: Object as () => Task, required: true },
+  },
+  setup() {
+    const me = ctfnote.me.injectMe();
+    const { result: team } = ctfnote.profiles.getTeam();
+
+    return {
+      me,
+      team,
+      startWorkingOn: ctfnote.tasks.useStartWorkingOn(),
+      stopWorkingOn: ctfnote.tasks.useStopWorkingOn(),
+      solveTask: injectStrict(keys.solveTaskPopup),
+      deleteTask: injectStrict(keys.deleteTaskPopup),
+      editTask: injectStrict(keys.editTaskPopup),
+    };
+  },
+
+  computed: {
+    onItColor() {
+      return this.onIt ? 'secondary' : 'primary';
+    },
+    onIt() {
+      if (!this.me.profile?.id) return false;
+      return this.task.workOnTasks.includes(this.me.profile.id);
+    },
+    onItIcon() {
+      return this.onIt ? 'person_remove' : 'person_add_alt_1';
+    },
+    onItTitle() {
+      return `${this.onIt ? 'Stop' : 'Start'} working on ${this.task.title}`;
+    },
+  },
+  methods: {
+    updateOnIt(v: boolean) {
+      if (v) {
+        void this.startWorkingOn(this.task);
+      } else {
+        void this.stopWorkingOn(this.task);
+      }
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ front/src/components/Task/TaskCategoryChip.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-chip
+    class="text-white"
+    clickable
+    :style="style"
+    @click="filterCategory(name)"
+  >
+    {{ name }}
+  </q-chip>
+</template>
+
+<script lang="ts">
+import ctfnote from 'src/ctfnote';
+import { injectStrict } from 'src/ctfnote/utils';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    name: { type: String, default: '?' },
+  },
+  setup() {
+    return {
+      filterCategory:
+        injectStrict<(category: string) => void>('filterCategory'),
+    };
+  },
+  computed: {
+    style() {
+      return { backgroundColor: ctfnote.utils.colorHash(this.name) };
+    },
+  },
+});
+</script>
+
+<style scoped></style>
