9641fb8d50b0c8f9d776ad4c6b3d1874f194c565
BitK <bitk@yeswehack.com>
2021-11-30 02:42:38 +0100

    Update format command to handle subdirectory
    Only the files in /src where formated with `yarn format`
    now the subfolder are also formated.
    
    + formating importCtf and savepointWrapper

--- api/src/plugins/savepointWrapper.ts
+++ api/src/plugins/savepointWrapper.ts
@@ -1,16 +1,19 @@
-import { Client } from "pg"
+import { Client } from "pg";
 
-async function savepointWrapper(pgClient: Client, f: () => void): Promise<void> {
-    const name = `"CHECKPOINT-${Math.floor(Math.random() * 0xffff)}"`
-    await pgClient.query(`SAVEPOINT ${name}`);
-    try {
-        await f()
-    } catch (e) {
-        await pgClient.query(`ROLLBACK TO SAVEPOINT ${name}`);
-        throw e;
-    } finally {
-        await pgClient.query(`RELEASE SAVEPOINT ${name}`);
-    }
+async function savepointWrapper(
+  pgClient: Client,
+  f: () => void
+): Promise<void> {
+  const name = `"CHECKPOINT-${Math.floor(Math.random() * 0xffff)}"`;
+  await pgClient.query(`SAVEPOINT ${name}`);
+  try {
+    await f();
+  } catch (e) {
+    await pgClient.query(`ROLLBACK TO SAVEPOINT ${name}`);
+    throw e;
+  } finally {
+    await pgClient.query(`RELEASE SAVEPOINT ${name}`);
+  }
 }
 
 export default savepointWrapper;
