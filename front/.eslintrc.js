95ccfdd6fea087c8e10a64c8396784e1064aaf24
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2023-03-24 20:33:09 +0100

    Upgrade packages of frontend to latest versions
    The SCSS variables are from https://github.com/quasarframework/quasar/issues/15144#issue-1502838365
    This fixes the ugly dark shadows in dark mode but keeps the shadows in light mode.
    This was added in Quasar 2.11 but reverted using the SCSS patch.
    
    Due to an update of ESLint, it complains about single-word components.
    Since this is used in CTFNote, this rule is disabeld.
    
    Also some small (automatic) fixes.

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -86,6 +86,7 @@ module.exports = {
     quotes: ['warn', 'single', { avoidEscape: true }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/multi-word-component-names': 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
