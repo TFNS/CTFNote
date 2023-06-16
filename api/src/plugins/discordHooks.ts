64ff2dc0adeae22b0091e20b48d429fe7ce787aa
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2023-06-16 21:07:47 +0200

    Fix hook name

--- api/src/plugins/discordHooks.ts
+++ api/src/plugins/discordHooks.ts
@@ -14,177 +14,172 @@ import {
 import { getDiscordClient, usingDiscordBot } from "../discord";
 import config from "../config";
 
-const discordMutationLoggingHook =
-  (_build: Build) => (fieldContext: Context<any>) => {
-    const {
-      scope: { isRootMutation },
-    } = fieldContext;
+const discordMutationHook = (_build: Build) => (fieldContext: Context<any>) => {
+  const {
+    scope: { isRootMutation },
+  } = fieldContext;
 
-    if (!isRootMutation) return null;
+  if (!isRootMutation) return null;
 
-    if (!usingDiscordBot) return null;
+  if (!usingDiscordBot) return null;
 
-    if (
-      fieldContext.scope.fieldName !== "updateTask" &&
-      fieldContext.scope.fieldName !== "createTask" &&
-      fieldContext.scope.fieldName !== "deleteTask" &&
-      fieldContext.scope.fieldName !== "startWorkingOn" &&
-      fieldContext.scope.fieldName !== "stopWorkingOn"
-    ) {
+  if (
+    fieldContext.scope.fieldName !== "updateTask" &&
+    fieldContext.scope.fieldName !== "createTask" &&
+    fieldContext.scope.fieldName !== "deleteTask" &&
+    fieldContext.scope.fieldName !== "startWorkingOn" &&
+    fieldContext.scope.fieldName !== "stopWorkingOn"
+  ) {
+    return null;
+  }
+
+  const handleDiscordMutationLog = async (
+    input: any,
+    args: any,
+    context: any
+  ) => {
+    const discordClient = getDiscordClient();
+    if (discordClient === null) return null;
+
+    const guild = discordClient.guilds.resolve(config.discord.serverId);
+
+    if (guild === null) {
+      console.error("Guild not found");
       return null;
     }
 
-    const handleDiscordMutationLog = async (
-      input: any,
-      args: any,
-      context: any
-    ) => {
-      const discordClient = getDiscordClient();
-      if (discordClient === null) return null;
+    //add challenges to the ctf channel discord
+    if (fieldContext.scope.fieldName === "createTask") {
+      const ctfName = await getCTFNameFromId(args.input.ctfId);
 
-      const guild = discordClient.guilds.resolve(config.discord.serverId);
+      const categoryChannel = guild?.channels.cache.find(
+        (channel) =>
+          channel.type === ChannelType.GuildCategory && channel.name === ctfName
+      ) as CategoryChannel | undefined;
 
-      if (guild === null) {
-        console.error("Guild not found");
+      if (categoryChannel === undefined) {
         return null;
       }
 
-      //add challenges to the ctf channel discord
-      if (fieldContext.scope.fieldName === "createTask") {
-        const ctfName = await getCTFNameFromId(args.input.ctfId);
+      categoryChannel.guild.channels
+        .create({
+          name: `${args.input.title} - ${args.input.category}`,
+          type: ChannelType.GuildText,
+          parent: categoryChannel.id,
+        })
+        .catch((err) => {
+          console.error("Failed creating category.", err);
+        });
 
-        const categoryChannel = guild?.channels.cache.find(
-          (channel) =>
-            channel.type === ChannelType.GuildCategory &&
-            channel.name === ctfName
-        ) as CategoryChannel | undefined;
+      //send message to the main channel that a new task has been created
+      const mainChannel = guild?.channels.cache.find(
+        (channel) =>
+          channel.type === ChannelType.GuildText &&
+          channel.name === "challenges-talk" &&
+          channel.parentId === categoryChannel.id
+      ) as TextChannel | undefined;
 
-        if (categoryChannel === undefined) {
-          return null;
-        }
-
-        categoryChannel.guild.channels
-          .create({
-            name: `${args.input.title} - ${args.input.category}`,
-            type: ChannelType.GuildText,
-            parent: categoryChannel.id,
-          })
+      if (mainChannel !== undefined) {
+        mainChannel
+          .send(
+            `New task created: ${args.input.title} - ${args.input.category}`
+          )
           .catch((err) => {
-            console.error("Failed creating category.", err);
+            console.error("Failed to send notification about a new task.", err);
           });
-
-        //send message to the main channel that a new task has been created
-        const mainChannel = guild?.channels.cache.find(
-          (channel) =>
-            channel.type === ChannelType.GuildText &&
-            channel.name === "challenges-talk" &&
-            channel.parentId === categoryChannel.id
-        ) as TextChannel | undefined;
-
-        if (mainChannel !== undefined) {
-          mainChannel
-            .send(
-              `New task created: ${args.input.title} - ${args.input.category}`
-            )
-            .catch((err) => {
-              console.error(
-                "Failed to send notification about a new task.",
-                err
-              );
-            });
-        }
       }
-      if (fieldContext.scope.fieldName === "deleteTask") {
-        const task = await getTaskFromId(args.input.id);
+    }
+    if (fieldContext.scope.fieldName === "deleteTask") {
+      const task = await getTaskFromId(args.input.id);
 
+      const taskTitle = task[0].toLowerCase();
+      const taskCategory = task[1].toLowerCase();
+
+      const channel = guild?.channels.cache.find(
+        (channel) =>
+          channel.type === ChannelType.GuildText &&
+          channel.name === `${taskTitle}-${taskCategory}`
+      ) as CategoryChannel | undefined;
+
+      if (channel === undefined) return null;
+
+      channel
+        .setName(`${taskTitle}-${taskCategory}-deleted`)
+        .catch((err) =>
+          console.error("Failed to mark channel as deleted.", err)
+        );
+    }
+
+    if (
+      fieldContext.scope.fieldName === "updateTask" &&
+      args.input.patch.flag !== null &&
+      args.input.id !== null
+    ) {
+      if (args.input.patch.flag !== "") {
+        const task = await getTaskFromId(args.input.id);
         const taskTitle = task[0].toLowerCase();
         const taskCategory = task[1].toLowerCase();
+        sendMessageFromTaskId(args.input.id, `${taskTitle} is solved!`)
+          .then(async (channel) => {
+            if (channel !== null) {
+              return channel.setName(`${taskTitle}-${taskCategory}-solved`);
+            }
+          })
+          .catch((err) => {
+            console.error("Failed sending solved notification.", err);
+          });
+      }
+    }
+    if (fieldContext.scope.fieldName === "startWorkingOn") {
+      //send a message to the channel that the user started working on the task
+      const userId = context.jwtClaims.user_id;
+      const taskId = args.input.taskId;
 
-        const channel = guild?.channels.cache.find(
-          (channel) =>
-            channel.type === ChannelType.GuildText &&
-            channel.name === `${taskTitle}-${taskCategory}`
-        ) as CategoryChannel | undefined;
-
-        if (channel === undefined) return null;
-
-        channel
-          .setName(`${taskTitle}-${taskCategory}-deleted`)
-          .catch((err) =>
-            console.error("Failed to mark channel as deleted.", err)
+      getNameFromUserId(userId)
+        .then((username) => {
+          return sendMessageFromTaskId(
+            taskId,
+            `${username} is working on this task!`
           );
-      }
+        })
+        .catch((err) => {
+          console.error("Failed sending 'working on' notification.", err);
+        });
+    }
+    if (fieldContext.scope.fieldName === "stopWorkingOn") {
+      //send a message to the channel that the user stopped working on the task
+      const userId = context.jwtClaims.user_id;
+      const taskId = args.input.taskId;
 
-      if (
-        fieldContext.scope.fieldName === "updateTask" &&
-        args.input.patch.flag !== null &&
-        args.input.id !== null
-      ) {
-        if (args.input.patch.flag !== "") {
-          const task = await getTaskFromId(args.input.id);
-          const taskTitle = task[0].toLowerCase();
-          const taskCategory = task[1].toLowerCase();
-          sendMessageFromTaskId(args.input.id, `${taskTitle} is solved!`)
-            .then(async (channel) => {
-              if (channel !== null) {
-                return channel.setName(`${taskTitle}-${taskCategory}-solved`);
-              }
-            })
-            .catch((err) => {
-              console.error("Failed sending solved notification.", err);
-            });
-        }
-      }
-      if (fieldContext.scope.fieldName === "startWorkingOn") {
-        //send a message to the channel that the user started working on the task
-        const userId = context.jwtClaims.user_id;
-        const taskId = args.input.taskId;
+      getNameFromUserId(userId)
+        .then((username) => {
+          return sendMessageFromTaskId(
+            taskId,
+            `${username} stopped working on this task!`
+          );
+        })
+        .catch((err) => {
+          console.error(
+            "Failed sending 'stopped working on' notification.",
+            err
+          );
+        });
+    }
 
-        getNameFromUserId(userId)
-          .then((username) => {
-            return sendMessageFromTaskId(
-              taskId,
-              `${username} is working on this task!`
-            );
-          })
-          .catch((err) => {
-            console.error("Failed sending 'working on' notification.", err);
-          });
-      }
-      if (fieldContext.scope.fieldName === "stopWorkingOn") {
-        //send a message to the channel that the user stopped working on the task
-        const userId = context.jwtClaims.user_id;
-        const taskId = args.input.taskId;
-
-        getNameFromUserId(userId)
-          .then((username) => {
-            return sendMessageFromTaskId(
-              taskId,
-              `${username} stopped working on this task!`
-            );
-          })
-          .catch((err) => {
-            console.error(
-              "Failed sending 'stopped working on' notification.",
-              err
-            );
-          });
-      }
-
-      return input;
-    };
-
-    return {
-      before: [],
-      after: [
-        {
-          priority: 500,
-          callback: handleDiscordMutationLog,
-        },
-      ],
-      error: [],
-    };
+    return input;
   };
+
+  return {
+    before: [],
+    after: [
+      {
+        priority: 500,
+        callback: handleDiscordMutationLog,
+      },
+    ],
+    error: [],
+  };
+};
 
 async function sendMessageFromTaskId(
   id: bigint,
@@ -224,7 +219,7 @@ async function sendMessageFromTaskId(
 
 export default function (builder: SchemaBuilder): void {
   builder.hook("init", (_, build) => {
-    build.addOperationHook(discordMutationLoggingHook(build));
+    build.addOperationHook(discordMutationHook(build));
     return _;
   });
 }
