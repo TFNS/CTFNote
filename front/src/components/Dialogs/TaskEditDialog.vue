3d50156a0601c1e125c963db45fde9a2728212ca
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2023-06-23 13:41:43 +0200

    Fix another linting warning (unrelated to Discord)

--- front/src/components/Dialogs/TaskEditDialog.vue
+++ front/src/components/Dialogs/TaskEditDialog.vue
@@ -17,12 +17,12 @@
             v-model="form.tags"
             label="Tags"
             :options="suggestions"
-            @filter="filterFn"
             input-debounce="0"
             use-input
             use-chips
             multiple
             new-value-mode="add-unique"
+            @filter="filterFn"
           />
         </q-card-section>
         <q-card-section class="q-pt-none">
