45de91260a6a1b666a8667051f50604967451b88
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2023-06-23 10:35:02 +0200

    Don't update profile picture in development
    This will stop the rate limiting errors when quickly developing.

--- api/src/discord/listeners/ready.ts
+++ api/src/discord/listeners/ready.ts
@@ -3,6 +3,7 @@ import { ActivityType, Client } from "discord.js";
 import { Commands } from "../commands";
 import interactionCreate from "./interactionCreate";
 import fs from "fs";
+import config from "../../config";
 
 export default (client: Client): void => {
   client.on("ready", async () => {
@@ -14,13 +15,15 @@ export default (client: Client): void => {
       activities: [{ name: "CTFs", type: ActivityType.Playing }],
     });
 
-    client.user
-      ?.setAvatar(
-        fs.existsSync("src/discord/boticon.png")
-          ? "src/discord/boticon.png"
-          : "dist/discord/boticon.png"
-      )
-      .catch((err) => console.error("Failed to change avatar of bot.", err));
+    if (config.env !== "development") {
+      client.user
+        ?.setAvatar(
+          fs.existsSync("src/discord/boticon.png")
+            ? "src/discord/boticon.png"
+            : "dist/discord/boticon.png"
+        )
+        .catch((err) => console.error("Failed to change avatar of bot.", err));
+    }
 
     await client.application.commands.set(Commands);
     interactionCreate(client);
