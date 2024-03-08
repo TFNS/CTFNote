1ad4c6c91d823532a33c1893d17f36deb6a37186
markfijneman <48481393+markfijneman@users.noreply.github.com>
2024-03-08 17:51:44 +0100

    Make CTF logo unclickable when url is not set

--- front/src/components/CTF/LogoLink.vue
+++ front/src/components/CTF/LogoLink.vue
@@ -1,14 +1,23 @@
 <template>
   <q-btn
-    :href="ctf.ctfUrl"
+    :href="ctf.ctfUrl || null"
     target="_blank"
     class="logo text-white"
+    :class="{ 'no-hover': !ctf.ctfUrl }"
     type="a"
     :style="style"
     :icon="icon"
+    :ripple="false"
     round
   >
-    <q-tooltip>Browse CTF website</q-tooltip>
+    <q-tooltip>
+      <span v-if="ctf.ctfUrl">
+        Browse CTF website
+      </span>
+      <span v-else>
+        No CTF website set
+      </span>
+    </q-tooltip>
   </q-btn>
 </template>
 
@@ -37,10 +46,18 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.logo {
-  background: var(--logo-url);
-  background-size: cover;
-  background-origin: 50% 50%;
-  background-position: center;
-}
+  .logo {
+    background: var(--logo-url);
+    background-size: cover;
+    background-origin: 50% 50%;
+    background-position: center;
+  }
+
+  .no-hover {
+    cursor: default;
+  }
+
+  .no-hover >>> .q-focus-helper {
+    display: none;
+  }
 </style>
