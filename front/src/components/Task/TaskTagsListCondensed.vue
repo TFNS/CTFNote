e2e422099be78025b53f08f7858e117f9c25b0c5
markfijneman <48481393+markfijneman@users.noreply.github.com>
2024-03-13 19:38:10 +0100

    Added condensed tag list component

---
+++ front/src/components/Task/TaskTagsListCondensed.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="row">
+    <task-tag-chip
+      v-if="first_tag !== undefined"
+      :name="first_tag.tag"
+    />
+
+    <q-chip
+      v-if="tags.length > 1"
+      text-color="white"
+      color="black"
+      class="non-selectable"
+    >
+      <q-tooltip
+        anchor="center right"
+        self="center left"
+        :offset="[0, 0]"
+        class="transparent"
+        transition-show="fade"
+        transition-hide="fade"
+      >
+        <q-card bordered style="border-radius: 46px !important;">
+          <q-card-section class="tooltip-section" style="padding: 4px;">
+            <task-tag-chip
+              v-for="tag in remaining_tags"  
+              :key="tag.nodeId"
+              :name="tag.tag"
+            />
+          </q-card-section>
+        </q-card>
+      </q-tooltip>
+      
+      <span class="tag-chip">
+        +{{ tags.length - 1 }}
+      </span>
+    </q-chip>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Tag } from 'src/ctfnote/models';
+import TaskTagChip from './TaskTagChip.vue';
+
+export default defineComponent({
+  components: {
+    TaskTagChip,
+  },
+  props: {
+    tags: { type: Object as () => Tag[], required: true },
+  },
+  setup() {
+    return {};
+  },
+  computed: {
+    first_tag: function () {
+      return this.tags.length > 0 ? this.tags[0] : undefined;
+    },
+    remaining_tags: function () {
+      return this.tags.slice(1);
+    },
+  }
+});
+</script>
