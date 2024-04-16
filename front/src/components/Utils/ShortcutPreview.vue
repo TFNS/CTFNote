5a7e05ee13446a97bcde07d04351b5296ca529f3
markfijneman <48481393+markfijneman@users.noreply.github.com>
2024-04-16 23:58:31 +0200

    Added component to preview key combinations for shortcuts

---
+++ front/src/components/Utils/ShortcutPreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <template v-for="(key, index) in keys" :key="key">
+    <q-chip square dense class="keycap">
+      {{ key }}
+    </q-chip>
+    <template v-if="index < keys.length - 1"> + </template>
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    keys: { type: Array, required: true },
+  },
+});
+</script>
+
+<style scoped>
+.keycap {
+  background-color: rgba(200, 200, 200, 0.4);
+}
+</style>
