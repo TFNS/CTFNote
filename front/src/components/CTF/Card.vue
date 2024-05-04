d02751829b7e990e36ef9819f0881987551a41bc
BitK <luk.philippe@gmail.com>
2024-05-04 20:23:36 +0200

    Change ctfcard layout

--- front/src/components/CTF/Card.vue
+++ front/src/components/CTF/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card bordered class="ctfcard">
+  <q-card bordered class="ctf-card" :style="style">
     <card-admin-menu :ctf="ctf" />
     <q-linear-progress
       v-if="progress >= 0"
@@ -7,46 +7,82 @@
       animation-speed="0"
       color="positive"
     />
-    <q-card-section>
-      <div class="row q-gutter-md items-center">
-        <div class="col-auto">
-          <logo-link :ctf="ctf" />
-        </div>
-        <div class="col-auto">
-          <ctf-note-link name="ctf" :ctf="ctf" :label="ctf.title">
-            <q-btn flat :label="ctf.title" />
-          </ctf-note-link>
-        </div>
-        <div class="col-auto">
-          <q-badge v-if="running" color="positive" class="running">
-            LIVE
-          </q-badge>
-        </div>
+    <q-card-section class="ctf-card-header">
+      <logo-link :ctf="ctf" class="ctf-card-header-logo" />
+      <ctf-note-link
+        name="ctf"
+        :ctf="ctf"
+        :label="ctf.title"
+        class="ctf-card-header-title"
+      >
+        <q-btn flat class="full-width text-left">
+          <div class="ellipsis">
+            {{ ctf.title }}
+          </div>
+        </q-btn>
+      </ctf-note-link>
+      <q-chip
+        v-if="running"
+        square
+        :size="$q.screen.lt.sm ? 'md' : 'sm'"
+        color="positive"
+        class="ctf-card-header-live"
+      >
+        LIVE
+      </q-chip>
 
-        <q-space />
-        <template v-if="ctf.ctftimeUrl">
-          <div class="col-auto">
-            <weight-badge :ctf="ctf" />
-          </div>
-          <div class="col-auto">
-            <ctf-time-link :ctf="ctf" />
-          </div>
-        </template>
-      </div>
+      <weight-badge :ctf="ctf" class="ctf-card-header-weight" />
+      <ctf-time-link :ctf="ctf" class="ctf-card-header-ctftime" />
     </q-card-section>
     <q-separator inset />
     <q-card-section>
-      <div class="row justify-between q-col-gutter-md">
-        <div class="text-justify col-sm">
-          <q-markdown no-html :src="ctf.description" />
+      <div class="q-mb-md column q-col-gutter-md background-logo">
+        <div class="row q-gutter-md">
+          <div class="col-auto">
+            <q-chip
+              color="primary"
+              text-color="white"
+              class="q-my-none"
+              :ripple="false"
+            >
+              <span class="text-weight-bold">Start:</span>&nbsp;
+              {{ startsAt }}
+            </q-chip>
+          </div>
+          <div class="col-auto">
+            <q-chip
+              color="primary"
+              text-color="white"
+              class="q-my-none"
+              :ripple="false"
+            >
+              <span class="text-weight-bold">End:</span>&nbsp;
+              {{ endsAt }}
+            </q-chip>
+          </div>
+          <a
+            v-if="ctf.ctfUrl"
+            class="col-auto"
+            :href="ctf.ctfUrl"
+            target="_blank"
+          >
+            <q-chip
+              clickable
+              color="primary"
+              text-color="white"
+              class="q-my-none"
+              :ripple="false"
+            >
+              <q-icon name="link" class="q-mr-sm" />
+              {{ ctfDomain || ctf.ctfUrl }}
+            </q-chip>
+          </a>
         </div>
-        <div class="col-auto col-grow">
-          <q-date
-            today-btn
-            :title="startDate"
-            :subtitle="startTime"
-            :model-value="dateRange"
-            range
+        <div class="col">
+          <q-markdown
+            no-html
+            :src="ctf.description"
+            class="ctf-card-description"
           />
         </div>
       </div>
@@ -64,10 +100,10 @@
   </q-card>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { date } from 'quasar';
 import { Ctf } from 'src/ctfnote/models';
-import { defineComponent, onMounted, onUnmounted, ref } from 'vue';
+import { computed } from 'vue';
 import BtnDelete from '../CTF/BtnDelete.vue';
 import BtnEdit from '../CTF/BtnEdit.vue';
 import CtfTimeLink from '../CTF/CtfTimeLink.vue';
@@ -75,64 +111,48 @@ import LogoLink from '../CTF/LogoLink.vue';
 import WeightBadge from '../CTF/WeightBadge.vue';
 import CtfNoteLink from '../Utils/CtfNoteLink.vue';
 import CardAdminMenu from './CardAdminMenu.vue';
+import { useNow } from 'src/composables/useNow';
 
-export default defineComponent({
-  components: {
-    LogoLink,
-    WeightBadge,
-    CtfTimeLink,
-    BtnEdit,
-    BtnDelete,
-    CardAdminMenu,
-    CtfNoteLink,
-  },
-  props: { ctf: { type: Object as () => Ctf, required: true } },
-  setup() {
-    const now = ref(new Date());
-    let interval = 0;
-    const watcher = () => {
-      return (now.value = new Date());
-    };
-    onMounted(() => {
-      interval = window.setInterval(watcher, 1000);
-    });
-    onUnmounted(() => {
-      window.clearInterval(interval);
-    });
-    return { now };
-  },
-  computed: {
-    running(): boolean {
-      return this.ctf.startTime < this.now && this.ctf.endTime > this.now;
-    },
-    dateRange() {
-      const startDate = date.formatDate(this.ctf.startTime, 'YYYY/MM/DD');
-      const endDate = date.formatDate(this.ctf.endTime, 'YYYY/MM/DD');
-      // If it's only one day return only the start
-      if (startDate == endDate) {
-        return startDate;
-      } else {
-        return {
-          from: startDate,
-          to: endDate,
-        };
-      }
-    },
-    startTime() {
-      return date.formatDate(this.ctf.startTime, 'HH:mm');
-    },
-    startDate() {
-      return date.formatDate(this.ctf.startTime, 'YYYY/MM/DD');
-    },
-    progress() {
-      const start = this.ctf.startTime.getTime();
-      const end = this.ctf.endTime.getTime();
-      const duration = end - start;
-      const elapsed = this.now.valueOf() - start;
-      const progress = Math.min(elapsed / duration, 1);
-      return progress;
-    },
-  },
+const props = defineProps<{
+  ctf: Ctf;
+}>();
+
+const now = useNow();
+
+const running = computed(
+  () => props.ctf.startTime < now.value && props.ctf.endTime > now.value
+);
+const startsAt = computed(() =>
+  date.formatDate(props.ctf.startTime, 'YYYY-MM-DD HH:mm')
+);
+
+const endsAt = computed(() =>
+  date.formatDate(props.ctf.endTime, 'YYYY-MM-DD HH:mm')
+);
+
+const progress = computed(() => {
+  const start = props.ctf.startTime.getTime();
+  const end = props.ctf.endTime.getTime();
+  const duration = end - start;
+  const elapsed = now.value.valueOf() - start;
+  const progress = Math.min(elapsed / duration, 1);
+  return progress;
+});
+
+const ctfDomain = computed(() => {
+  try {
+    const url = new URL(props.ctf?.ctfUrl ?? '');
+    return url.hostname;
+  } catch {
+    return '';
+  }
+});
+
+const style = computed(() => {
+  if (props.ctf.logoUrl) {
+    return { '--bgUrl': `url(${props.ctf.logoUrl})` };
+  }
+  return {};
 });
 </script>
 
@@ -146,7 +166,77 @@ export default defineComponent({
   }
 }
 
-.running {
-  animation: blinker 0.5s ease-in infinite alternate;
+.ctf-card {
+  --description-width: 800px;
+  max-width: 100%;
+
+  .background-logo {
+    &::before {
+      content: '';
+      background: var(--bgUrl);
+      background-position: 100% 50%;
+      background-size: contain;
+      background-repeat: no-repeat;
+      opacity: 0.3;
+      width: calc(100% - var(--description-width) - 48px);
+      height: 100%;
+      right: 16px;
+      position: absolute;
+      pointer-events: none;
+    }
+  }
+
+  .ctf-card-description {
+    max-width: var(--description-width);
+    text-align: justify;
+    min-height: 132px;
+  }
+
+  .ctf-card-header {
+    display: grid;
+    grid-template-areas: 'logo  title live space weight ctftime';
+    grid-template-columns: auto auto auto 1fr auto auto auto;
+    gap: 8px;
+    align-items: center;
+
+    @media (max-width: $breakpoint-sm-min) {
+      grid-template-areas: 'title title title title title' 'logo live space weight ctftime';
+      grid-template-columns: auto auto 1fr auto auto;
+    }
+
+    .ctf-card-header-logo {
+      grid-area: logo;
+    }
+
+    .ctf-card-header-title {
+      grid-area: title;
+      max-width: 100%;
+      min-width: 0;
+
+      @media (max-width: $breakpoint-sm-min) {
+        width: 100%;
+      }
+    }
+
+    .ctf-card-header-weight {
+      grid-area: weight;
+    }
+
+    .ctf-card-header-ctftime {
+      grid-area: ctftime;
+      --offset: 32px;
+
+      @media (max-width: $breakpoint-sm-min) {
+        --offset: 24px;
+      }
+      transform: translateX(var(--offset));
+      margin-left: calc(-1 * var(--offset));
+    }
+
+    .ctf-card-header-live {
+      grid-area: live;
+      animation: blinker 0.5s ease-in infinite alternate;
+    }
+  }
 }
 </style>
