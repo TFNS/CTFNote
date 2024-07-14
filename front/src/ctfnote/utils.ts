fd9dbcf084aa9603e50713e11a11c1d8447d1bd9
JJ-8 <34778827+JJ-8@users.noreply.github.com>
2024-07-14 21:19:48 +0200

    Fix rendering issues with wrapQuery
    The refactoring of onResult gave a white page for me so I rewritten it to solve the error.

--- front/src/ctfnote/utils.ts
+++ front/src/ctfnote/utils.ts
@@ -2,7 +2,7 @@ import { OperationVariables } from '@apollo/client';
 import { UseQueryReturn } from '@vue/apollo-composable';
 import ColorHash from 'color-hash';
 import { DeepNonNullable, DeepRequired } from 'ts-essentials';
-import { computed, inject, InjectionKey } from 'vue';
+import { computed, inject, InjectionKey, UnwrapRef } from 'vue';
 
 export function wrapQuery<D, T, U extends OperationVariables>(
   query: UseQueryReturn<T, U>,
@@ -10,10 +10,13 @@ export function wrapQuery<D, T, U extends OperationVariables>(
   wrapper: (data: DeepRequired<DeepNonNullable<T>>) => D
 ) {
   const result = computed(() => {
-    if (query.result) {
-      return wrapper(query.result as DeepRequired<DeepNonNullable<T>>);
+    if (query.result.value) {
+      return wrapper(
+        query.result.value as DeepRequired<DeepNonNullable<T>>
+      ) as UnwrapRef<D>;
+    } else {
+      return def as UnwrapRef<D>;
     }
-    return def;
   });
 
   const onResult = function (cb: (arg: D) => void) {
