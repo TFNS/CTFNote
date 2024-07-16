d66de9558507485df75a8177af266eb06b7a79f2
Daan Breur <git@daanbreur.systems>
2024-07-16 23:21:09 +0200

    Maybe its a good idea to grant the correct user....

--- api/migrations/54-add-discord-integration-enabled.sql
+++ api/migrations/54-add-discord-integration-enabled.sql
@@ -1,7 +1,7 @@
 ALTER TABLE ctfnote.settings
   ADD COLUMN "discord_integration_enabled" boolean NOT NULL DEFAULT FALSE;
 
-GRANT SELECT ("discord_integration_enabled") ON ctfnote.settings TO user_guest;
+GRANT SELECT ("discord_integration_enabled") ON ctfnote.settings TO user_anonymous;
 REVOKE UPDATE ("discord_integration_enabled") ON ctfnote.settings FROM user_admin;
 GRANT UPDATE ("discord_integration_enabled") ON ctfnote.settings TO user_postgraphile;
 
